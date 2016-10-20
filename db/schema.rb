5e0ac911637926d25742558b06cf811404111269
keita sato <aaa.keita.bbb@gmail.com>
2016-10-20 12:45:27 +0000

    login image

--- db/schema.rb
+++ db/schema.rb
@@ -11,79 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-=======
-ActiveRecord::Schema.define(version: 20161020110004) do
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+ActiveRecord::Schema.define(version: 20161020123752) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -91,11 +19,6 @@ ActiveRecord::Schema.define(version: 20161020110004) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "joins", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "event_id"
   end
 
   create_table "events", force: :cascade do |t|
@@ -108,16 +31,41 @@ ActiveRecord::Schema.define(version: 20161020110004) do
     t.string   "area"
     t.integer  "day"
     t.integer  "capacity"
- 
->>>>>>> bd918a28eba8bb0294194250aa31a6d2a12ef315
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-   create_table "posts", force: :cascade do |t|
+  create_table "joins", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.string   "image"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end

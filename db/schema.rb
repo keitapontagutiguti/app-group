811269da5ba1cc26f8409ea3df914f0ccdc7c32a
keita sato <aaa.keita.bbb@gmail.com>
2016-10-21 15:44:18 +0000

    user nationality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021145847) do
+ActiveRecord::Schema.define(version: 20161021153542) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20161021145847) do
     t.string   "image"
     t.integer  "gender"
     t.text     "inform"
+    t.string   "nationality"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
